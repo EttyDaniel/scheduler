13fc2f8024cd36c75f0696df561b63edbf83b4fe
EttyDaniel <danieleti@gmail.com>
2021-11-18 20:36:06 +0000

    adding dayList component to application

---
+++ src/components/DayList.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "components/DayListItem.scss";
+import DayListItem from "./DayListItem";
+import classNames from "classnames";
+
+export default function DayList(props) {
+
+  let daysArray = [];
+  return (
+    <ul>
+      {props.days.map((day)=> {
+        return <DayListItem 
+        key={day.id}
+        name={day.name} 
+        spots={day.spots} 
+        selected={day.name === props.day}
+        setDay={props.setDay}  
+      />
+      })}
+      
+    </ul>
+  );
+}
