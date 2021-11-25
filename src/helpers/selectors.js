36d8148169d29a95a021e383dd7acd1fefd46b6e
EttyDaniel <danieleti@gmail.com>
2021-11-25 11:23:53 -0500

    adding selectors and getAppointmentByDay function

---
+++ src/helpers/selectors.js
@@ -0,0 +1,17 @@
+
+
+export function getAppointmentsForDay(state, day) {
+  let appointmentsArr = [];
+  let appointmentsByDay = [];
+  for(const stateDay of state.days) {
+    if(stateDay.name === day){
+      appointmentsByDay = stateDay.appointments;
+    }
+  }
+  if(appointmentsByDay) {
+    for(const appointmentId of appointmentsByDay) {
+      appointmentsArr.push(state.appointments[appointmentId]);
+    }
+  }
+  return appointmentsArr;
+}
