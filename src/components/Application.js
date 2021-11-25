268c8f4ff19bff3ba539001f4bdf58e6c8e12088
EttyDaniel <danieleti@gmail.com>
2021-11-24 22:30:58 -0500

    using useState and axios to fetch data from api and store in state

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,27 +1,10 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Appointment from "./Appointment";
+import axios from "axios";
 
 import "components/Application.scss";
 import DayList from "./DayList";
 import "components/Appointment";
-
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
 
 const appointments = {
   "1": {
@@ -66,6 +49,13 @@ const appointments = {
 export default function Application(props) {
 
   const [day, setDay] = useState("Monday");
+  const [days, setDays] = useState([]);
+
+  useEffect(() => {axios.get("/api/days").then(response => {
+    console.log("response: ", response);
+    setDays(response.data);
+  });
+  }, []);
 
   return (
     <main className="layout">
