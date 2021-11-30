57fa79a4d67808386900816805a4c811626b328e
EttyDaniel <danieleti@gmail.com>
2021-11-30 12:08:34 -0500

    editing appointment component and using useVisualMode custome hook

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,14 +3,28 @@ import "components/Appointment/styles.scss";
 import Empty from "components/Appointment/Empty.js";
 import Header from "components/Appointment/Header.js";
 import Show from "components/Appointment/Show.js";
+import useVisualMode from "hooks/useVisualMode";
+import Form from "./Form";
 
 export default function Appointment(props) {
   const interview = props.interview;
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const CREATE = "CREATE";
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
   return (
     <article 
     className="appointment" id={props.id}>
       <Header time={props.time}/>
-      {interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer}/> : <Empty/>}
+      {/* {interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer}/> : <Empty/>} */}
+      {mode === EMPTY && <Empty onAdd={() => transition("CREATE")} />}
+      {mode === SHOW && (
+      <Show
+        student={props.interview.student}
+        interviewer={props.interview.interviewer}
+      />)}
+      {mode === CREATE && <Form interviewers={[]} onCancel={ () => { back() }}/>}
     </article>);
-  
 }
